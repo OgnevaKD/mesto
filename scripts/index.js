cef8d2b5fe69df73a08ea75bccef32747ff02124
Ksenia <ognevakd@gmail.com>
2023-07-21 16:06:34 +0300

    ПР4 после 1 ревью

---
+++ scripts/index.js
@@ -0,0 +1,34 @@
+const profileEdit = document.querySelector('.profile__edit-button');
+const popupEdit = document.querySelector('.popup');
+const buttonCloseEditPopup = popupEdit.querySelector('.popup__close-button');
+const formElement = document.querySelector('.popup__container');
+const nameInput = formElement.querySelector('.popup__input_type_name');
+const bioInput = formElement.querySelector('.popup__input_type_bio');
+const profileName = document.querySelector('.profile__name');
+const profileBio = document.querySelector('.profile__bio');
+
+function addPopup() {
+  popupEdit.classList.add('popup_opened');
+}
+
+function closePopup() {
+  popupEdit.classList.remove('popup_opened');
+}
+
+function handleFormSubmit(evt) {
+  evt.preventDefault();
+
+  profileName.textContent = nameInput.value;
+  profileBio.textContent = bioInput.value;
+
+  closePopup();
+}
+
+function closePopup() {
+  popupEdit.classList.remove('popup_opened');
+}
+
+profileEdit.addEventListener('click', addPopup);
+buttonCloseEditPopup.addEventListener('click', closePopup);
+
+formElement.addEventListener('submit', handleFormSubmit);
